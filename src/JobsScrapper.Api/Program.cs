5fff97f40c5ceaae99390e2c24cbd9247d99f135
eduardo-sepassos <eduardo.sepassos@gmail.com>
2026-05-03 20:50:08 -0300

    Enable Swagger in Production environment

--- src/JobsScrapper.Api/Program.cs
+++ src/JobsScrapper.Api/Program.cs
@@ -13,11 +13,8 @@ builder.Services.AddAplication();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.MapControllers();
